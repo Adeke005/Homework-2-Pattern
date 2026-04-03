6db26314790abb21e6c5b50358cc20da5c215ff6
Nazikey Murat <your_email@gmail.com>
2026-04-03 10:31:13 +0500

    handler

---
+++ src/main/java/rpg/chain/BlockHandler.java
@@ -0,0 +1,9 @@
+package rpg.chain;
+
+public class BlockHandler extends DefenseHandler {
+
+    public int handle(int damage) {
+        System.out.println("Blocked 5 damage");
+        return next.handle(damage - 5);
+    }
+}
